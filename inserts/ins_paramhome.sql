91a4abd09be7ecf8df153b8d85c1b29596ab21d6
root <johnmorenoing@gmail.com>
2015-12-08 17:52:40 -0500

    Insert para subcategorias del home

---
+++ inserts/ins_paramhome.sql
@@ -0,0 +1,6 @@
+insert into fa_tprodhome values(1,2,1,2,'zapata.png','A','ZAPATA');
+insert into fa_tprodhome values(2,2,2,2,'hexagonal.png','A','HEXAGONAL');
+insert into fa_tprodhome values(3,2,4,2,'argolla.png','A','ARGOLLA');
+insert into fa_tprodhome values(4,2,8,2,'chazo.png','A','CHAZO');
+insert into fa_tprodhome values(5,2,9,2,'esparrago.png','A','ESPARRAGO');
+
