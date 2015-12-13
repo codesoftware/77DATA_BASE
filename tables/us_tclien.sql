87e1220be839daa8d93960dfcd38b59aef70fb9a
jnsierra <jnsierrac@gmail.com>
2015-12-13 16:26:10 -0500

    13/12/2015 001
    Ajustes para mejorar el uso de clientes

--- tables/us_tclien.sql
+++ tables/us_tclien.sql
@@ -4,22 +4,24 @@
 --DROP TABLE IF EXISTS US_TCLIEN; 
 
 CREATE TABLE US_TCLIEN(
-   CLIEN_CLIEN            SERIAL                              ,   -- Llave primaria de la tabla
-   CLIEN_CEDULA           NUMERIC(10,0)       NOT NULL        ,   -- Llave foranea de la tabla us_tpers
-   CLIEN_NOMBRES          VARCHAR(50)         NOT NULL        ,
-   CLIEN_APELLIDOS        VARCHAR(50)                         ,
-   CLIEN_TELEFONO         VARCHAR(50)                         ,
-   CLIEN_CORREO           VARCHAR(50)                         ,   
-   CLIEN_DIRECCION        VARCHAR(50)                         ,   
+   CLIEN_CLIEN            SERIAL                               ,   -- Llave primaria de la tabla
+   CLIEN_CEDULA           VARCHAR(200)       NOT NULL          ,   -- Llave foranea de la tabla us_tpers
+   CLIEN_NOMBRES          VARCHAR(200)       NOT NULL          ,
+   CLIEN_APELLIDOS        VARCHAR(200)                         ,
+   CLIEN_TELEFONO         VARCHAR(200)                         ,
+   CLIEN_CORREO           VARCHAR(200)                         ,   
+   CLIEN_DIRECCION        VARCHAR(200)                         ,   
    PRIMARY KEY (CLIEN_CLIEN)
 );
-
+--
+--Tabla para el cargue temporal de clientes
+--
 CREATE TABLE us_tmpclien(
-tmpclien_tmpclien serial NOT NULL,
-tmpclien_nit      varchar(200) NOT NULL,
-tmpclien_nombre   varchar(2000) NOT NULL,
-tmpclien_direccion varchar(2000),
-tmpclien_telefono varchar(2000),
-tmpclien_ciudad  varchar(2000),
+    tmpclien_tmpclien           serial                NOT NULL   ,
+    tmpclien_nit                varchar(200)          NOT NULL   ,
+    tmpclien_nombre             varchar(2000)         NOT NULL   ,
+    tmpclien_direccion          varchar(2000)                    ,
+    tmpclien_telefono           varchar(2000)                    ,
+    tmpclien_ciudad             varchar(2000)                    ,
 PRIMARY KEY (tmpclien_tmpclien)
 );
