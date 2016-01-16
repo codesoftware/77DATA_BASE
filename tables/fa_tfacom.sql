5137d2fae31182d6082f9e4ab5710f4c6268941c
jmorenor1986 <johmorenoing@gmail.com>
2016-01-16 16:59:46 -0500

    Cambios adiciona productos por factura y retefuente

--- tables/fa_tfacom.sql
+++ tables/fa_tfacom.sql
@@ -1,8 +1,11 @@
 CREATE TABLE fa_tfacom (
 facom_facom             BIGSERIAL,
 facom_fecha             TIMESTAMP NOT NULL,
+facom_ferec             TIMESTAMP NOT NULL,
 facom_nufac				VARCHAR(200) NOT NULL,
 facom_valor				NUMERIC(1000,6) NOT NULL,
+facom_vliva				NUMERIC(1000,6) NOT NULL,
+facom_vlret				NUMERIC(1000,6) NOT NULL,
 facom_rtimg				VARCHAR(1000) NOT NULL,
 facom_tprov             BIGSERIAL,
 facom_estad             VARCHAR(2),
