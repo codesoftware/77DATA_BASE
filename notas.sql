90cdae55a6658b4a2d8dfe7da3cba344b6f5c60b
jnsierra <jnsierrac@gmail.com>
2016-01-23 13:16:24 -0500

    1
    Nota basica

--- notas.sql
+++ notas.sql
@@ -3,3 +3,5 @@ Notas:
 2. Despues de ejecutar el comand instalador se debe correr los insert que estan en clientes77.sql
 3. se debe ejecutar f_cargacliente();
 4. despues de subir los productos se debe ejecutar in_cambia_panel_pedidos()
+5. Cundo se realice el proceso de cargue por medio del excel no va ha ingresar ningun precio ni existencia,
+   se debe correr el proceso FA_INSERTA_PROD_MASIVO , despues de parametrizar todos los porcentajes de precios
