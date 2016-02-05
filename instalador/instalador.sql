41d39cd33c54966a6afb0c0398fca5da54612608
jnsierra <jnsierrac@gmail.com>
2016-02-04 19:42:37 -0500

    04/02/2016 001
    Cambio para ajuste en el instalador

--- instalador/instalador.sql
+++ instalador/instalador.sql
@@ -290,6 +290,7 @@
 \i /77DATA_BASE/functions/FA_VAL_AJU_AUTO_IN.sql;
 \i /77DATA_BASE/functions/IN_PARA_PORC_PRECIOS.sql;
 \i /77DATA_BASE/functions/IN_EJECUTA_PROC_PRECIOS.sql;
+\i /77DATA_BASE/functions/IN_EJECUTA_MASIVOS.sql;
 --
 -- Triggers
 --
