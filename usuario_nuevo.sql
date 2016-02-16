b80fcb118d9aeb55c061fb8e00302ebd3ec8dd96
jnsierra <jnsierrac@gmail.com>
2016-02-16 17:22:17 -0500

    1
    1

--- usuario_nuevo.sql
+++ usuario_nuevo.sql
@@ -10,3 +10,9 @@ ALTER SEQUENCE NOMBRE_SECUENCIA restart 1
 
 update in_tkapr
 set kapr_mvin = 2
+
+
+select * 
+from pg_stat_activity
+WHERE APPLICATION_NAME <> 'pgAdmin III - Browser'
+AND DATNAME = 'SigemcoPRUEBAS'
