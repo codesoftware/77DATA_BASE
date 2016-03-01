f46451fe6a72b77add783fb5c28204c45d3c5997
jnsierra <jnsierrac@gmail.com>
2016-02-29 19:18:47 -0500

    29/02/2016 001
    cambio de campo

--- tables/in_tprov.sql
+++ tables/in_tprov.sql
@@ -4,17 +4,17 @@
 --DROP TABLE IF EXISTS IN_TMVIN; 
 
 CREATE TABLE IN_TPROV(
-PROV_PROV                 BIGSERIAL                      ,
-PROV_NOMBRE               VARCHAR(200)    NOT NULL     ,
-PROV_NIT                  VARCHAR(200)    NOT NULL     ,
-PROV_RAZON_SOCIAL         VARCHAR(200)    NOT NULL     ,
-PROV_REPRESENTANTE        VARCHAR(200)    NOT NULL     ,
-PROV_TELEFONO             VARCHAR(20)    NOT NULL     ,
-PROV_DIRECCION            VARCHAR(50)    NOT NULL     ,
-PROV_CELULAR              VARCHAR(15)    NOT NULL     ,
-PROV_ESTADO               VARCHAR(2)     DEFAULT ('A'),
+PROV_PROV                 BIGSERIAL                                 ,
+PROV_NOMBRE               VARCHAR(200)          NOT NULL            ,
+PROV_NIT                  VARCHAR(200)          NOT NULL            ,
+PROV_RAZON_SOCIAL         VARCHAR(200)          NOT NULL            ,
+PROV_REPRESENTANTE        VARCHAR(200)          NOT NULL            ,
+PROV_TELEFONO             VARCHAR(20)           NOT NULL            ,
+PROV_DIRECCION            VARCHAR(50)           NOT NULL            ,
+PROV_CELULAR              VARCHAR(15)           NOT NULL            ,
+PROV_ESTADO               VARCHAR(2)            DEFAULT ('A')       ,
 prov_corre                VARCHAR(50),
-prov_retde                BIGSERIAL,
+prov_retde                bigint                DEFAULT 1           ,
 prov_gcron                VARCHAR(2),        
 PRIMARY KEY (PROV_PROV)
 );
