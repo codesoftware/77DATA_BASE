153d96ee5da2c477ff4f7d5848254b75e7002687
jnsierra <jnsierrac@gmail.com>
2016-03-28 13:54:20 -0500

    28/03/2016
    Cambio de comentario

--- functions/FA_ID_CONSULTA_NOTA.sql
+++ functions/FA_ID_CONSULTA_NOTA.sql
@@ -1,27 +1,26 @@
---	
---Funcion que consulta el id de la factura
+--
+--Funcion que consulta con el id de la factura el id de la nota credito.
 --
 CREATE OR REPLACE FUNCTION FA_ID_CONSULTA_NOTA(
-								p_idFac   BIGINT
-								)RETURNS BIGINT AS $$
-
-
+                                p_idFac   BIGINT
+                                )RETURNS BIGINT AS $$
 DECLARE 
-	c_con_id_fanc CURSOR IS 
-	SELECT COALESCE(FANC_ID,0) 
-	FROM FA_TFANC
-	WHERE FANC_FACT = p_idFac
-	;
-
-	v_id       BIGINT :=0;
-
+    --
+    c_con_id_fanc CURSOR IS 
+    SELECT COALESCE(FANC_ID,0) 
+    FROM FA_TFANC
+    WHERE FANC_FACT = p_idFac
+    ;
+    --
+    v_id       BIGINT :=0;
+    --
 BEGIN
-
-	OPEN c_con_id_fanc;
-	FETCH c_con_id_fanc INTO v_id;
-	CLOSE c_con_id_fanc;
-
-	RETURN v_id;
-
+    --
+    OPEN c_con_id_fanc;
+    FETCH c_con_id_fanc INTO v_id;
+    CLOSE c_con_id_fanc;
+    --
+    RETURN v_id;    
+    --
 END;
-$$ LANGUAGE 'plpgsql';	 
+$$ LANGUAGE 'plpgsql';
