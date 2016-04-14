906a31a0b87ed8456f1b5353bc723896005a6614
John Moreno <johnmorenoing@gmail.com>
2016-04-14 17:27:18 -0500

    14/04/2016 001
    cabios

--- alters/abril_2016.sql
+++ alters/abril_2016.sql
@@ -11,7 +11,36 @@ ALTER TABLE fa_tfact
 ADD fact_ajpeso NUMERIC(1000,10) NOT NULL DEFAULT 0
 ;
 
+<<<<<<< HEAD
 CREATE SEQUENCE sq_co_ttem_fact_rece;
 ALTER TABLE co_ttem_fact_rece ALTER COLUMN TEM_FACT_RECE SET NOT NULL;
 ALTER TABLE co_ttem_fact_rece ALTER COLUMN TEM_FACT_RECE SET DEFAULT nextval('sq_co_ttem_fact_rece');
 ALTER SEQUENCE sq_co_ttem_fact_rece OWNED BY co_ttem_fact_rece.TEM_FACT_RECE;
+=======
+--
+--Columna la cual se utilizara para indicar a que resolucion de facturacion pertenece la factura
+--
+ALTER TABLE fa_tfact 
+ADD fact_rsfa BIGINT NOT NULL DEFAULT 1
+;
+--
+--Columna la cual se utilizara para indicar el consecutivo de la factura dentro de la resolucion de facturacion
+--
+ALTER TABLE fa_tfact 
+ADD fact_cons NUMERIC(1000,10) NOT NULL DEFAULT 0
+;
+--
+--Llave con la cual garantizo la existencia de una resolucion de facturacion
+--
+ALTER TABLE FA_TFACT
+ADD FOREIGN KEY (fact_rsfa)
+REFERENCES fa_trsfa(rsfa_rsfa)
+;
+--
+--Llave con la cual se garantiza que no se repita el mismo consecutivo en una resolucion de facturacion
+--
+ALTER TABLE FA_TFACT
+ADD CONSTRAINT RSFA_UNIQUE 
+UNIQUE (fact_rsfa,fact_cons)
+;
+>>>>>>> origin/master
