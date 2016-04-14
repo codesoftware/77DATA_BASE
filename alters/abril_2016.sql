b61b8ce9157a5d77da13de76d4539afcd413c4ae
John Moreno <johnmorenoing@gmail.com>
2016-04-14 16:23:35 -0500

    14/04/2016 001
    alters

--- alters/abril_2016.sql
+++ alters/abril_2016.sql
@@ -10,3 +10,8 @@ CHECK (pedi_esta in ('CR','CA','FA','GU','CO','SR','RE'));
 ALTER TABLE fa_tfact 
 ADD fact_ajpeso NUMERIC(1000,10) NOT NULL DEFAULT 0
 ;
+
+CREATE SEQUENCE sq_co_ttem_fact_rece;
+ALTER TABLE co_ttem_fact_rece ALTER COLUMN TEM_FACT_RECE SET NOT NULL;
+ALTER TABLE co_ttem_fact_rece ALTER COLUMN TEM_FACT_RECE SET DEFAULT nextval('sq_co_ttem_fact_rece');
+ALTER SEQUENCE sq_co_ttem_fact_rece OWNED BY co_ttem_fact_rece.TEM_FACT_RECE;
