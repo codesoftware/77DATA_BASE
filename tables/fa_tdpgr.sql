50c0b95de058740b06979072afefcd18720f9730
jnsierra <jnsierrac@gmail.com>
2016-05-12 10:50:51 -0500

    12/05/2016 002
    Ajuste para que no falle

--- tables/fa_tdpgr.sql
+++ tables/fa_tdpgr.sql
@@ -7,9 +7,9 @@ CREATE TABLE fa_tdpgr(
     dpgr_fecha                  TIMESTAMP               NOT NULL DEFAULT now()      ,    
     dpgr_estado                 VARCHAR(2)              NOT NULL                    ,
     dpgr_tipopago               VARCHAR(2)              NOT NULL                    ,
-    dpgr_valor                  NUMERIC(1000,10)        NOT NULL
+    dpgr_valor                  NUMERIC(1000,10)        NOT NULL                    ,
     dpgr_comprobante            VARCHAR(1000)                                       ,
     dpgr_vlrdeuda               NUMERIC(1000,10)        NOT NULL                    ,
     dpgr_mvcot                  BIGINT                  NOT NULL                    ,
-PRIMARY KEY (pgrm_pgrm)
+PRIMARY KEY (dpgr_dpgr)
 );
