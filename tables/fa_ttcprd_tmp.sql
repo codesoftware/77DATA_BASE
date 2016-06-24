b234152202c2ac1fcad7fab3f100ee3773f5bb07
jmorenor1986 <johmorenoing@gmail.com>
2016-06-24 13:21:29 -0500

    cambio tipo de dato

---
+++ tables/fa_ttcprd_tmp.sql
@@ -0,0 +1,13 @@
+CREATE TABLE fa_tfcprd_tmp (
+fcprd_tmp_fcprd					BIGSERIAL,
+fcprd_tmp_facom					BIGINT,
+fcprd_tmp_dska					BIGINT,
+fcprd_tmp_cant					INT,
+fcprd_tmp_subt					NUMERIC(1000,6),
+fcprd_tmp_iva					NUMERIC(1000,6),
+fcprd_tmp_piva					NUMERIC(1000,6),
+fcprd_tmp_tota					NUMERIC(1000,6),
+fcprd_tmp_esta					VARCHAR(2) DEFAULT 'A',
+fcprd_tmp_fech					TIMESTAMP DEFAULT NOW(),
+PRIMARY KEY (fcprd_tmp_fcprd)
+);
