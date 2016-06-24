8347ef68a821cd212d2db70a0a98cedefaf79e36
jmorenor1986 <johmorenoing@gmail.com>
2016-06-24 10:43:40 -0500

    Cambios tabla proveedores

--- altersJunio.sql
+++ altersJunio.sql
@@ -13,3 +13,12 @@ add prov_ciu VARCHAR(200);
 --
 alter table in_tprov
 add prov_mpio VARCHAR(200);
+--
+--
+--digito de verificacion
+alter table in_tprov
+add prov_dver VARCHAR(200);
+---
+--dias credito
+alter table in_tprov
+add prov_dcre BIGINT;
