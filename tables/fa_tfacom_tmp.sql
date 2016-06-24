eb5f2030dc94d645777ff8726171890a9f24d683
jmorenor1986 <johmorenoing@gmail.com>
2016-06-24 17:18:43 -0500

    Arreglo tabla temporal

---
+++ tables/fa_tfacom_tmp.sql
@@ -0,0 +1,19 @@
+CREATE TABLE fa_tfacom_tmp (
+facom_tmp_facom                 BIGSERIAL NOT NULL,
+facom_tmp_fecha                 TIMESTAMP ,
+facom_tmp_ferec                 TIMESTAMP ,
+facom_tmp_nufac                 VARCHAR(200) ,
+facom_tmp_valor                 NUMERIC(1000,6),
+facom_tmp_vliva                 NUMERIC(1000,6),
+facom_tmp_vlret                 NUMERIC(1000,6),
+facom_tmp_rtimg                 VARCHAR(1000),
+facom_tmp_tprov                 BIGINT,
+facom_tmp_estad                 VARCHAR(2),
+facom_tmp_tius                  BIGINT,
+facom_tmp_sede                  BIGINT,
+facom_tmp_fecre                 TIMESTAMP,
+facom_tmp_ajus                  VARCHAR(1000),
+facom_tmp_plaz					BIGINT,
+
+ PRIMARY KEY(facom_tmp_facom)
+);
