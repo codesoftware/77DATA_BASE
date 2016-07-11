14ff15e8c530904991986c8b26cfdbc4d1be43bd
jnsierra <jnsierrac@gmail.com>
2016-07-10 21:27:05 -0500

    10/07/2016 003
    Ajustes para la suma de creditos y debitos

--- foreignKey/fk_im_tdgas.sql
+++ foreignKey/fk_im_tdgas.sql
@@ -5,3 +5,10 @@ ALTER TABLE im_tdgas
 ADD FOREIGN KEY (dgas_auco)
 REFERENCES co_tauco(auco_auco)
 ;
+--
+--Llave que referencia el gasto con sus detalles
+--
+ALTER TABLE im_tdgas
+ADD FOREIGN KEY (dgas_gast)
+REFERENCES im_tgast(gast_gast)
+;
--- triggers/im_tdgas_before_insert_or_update.sql
+++ triggers/im_tdgas_before_insert_or_update.sql
@@ -6,22 +6,35 @@ CREATE OR REPLACE FUNCTION f_suma_detalles() RETURNS trigger AS $f_suma_detalles
         --
         --Cursor con el cual sumo el total de los detalles de los gastos
         --
-        c_suma_detalles CURSOR FOR
+        c_suma_detalles_d CURSOR FOR
         SELECT sum(dgas_valor)
           FROM im_tdgas
          WHERE dgas_gast = NEW.dgas_gast
+           AND dgas_natu = 'D'
          ;
         --
-        v_valor_gast        NUMERIC(1000,10) := 0;
+        c_suma_detalles_c CURSOR FOR
+        SELECT sum(dgas_valor)
+          FROM im_tdgas
+         WHERE dgas_gast = NEW.dgas_gast
+           AND dgas_natu = 'C'
+         ;
+        --
+        v_valor_gast_d        NUMERIC(1000,10) := 0;
+        v_valor_gast_c        NUMERIC(1000,10) := 0;
         --
     BEGIN
         --
-        OPEN c_suma_detalles;
-        FETCH c_suma_detalles INTO v_valor_gast;
-        CLOSE c_suma_detalles;
+        OPEN c_suma_detalles_d;
+        FETCH c_suma_detalles_d INTO v_valor_gast_d;
+        CLOSE c_suma_detalles_d;
+        --
+        OPEN c_suma_detalles_c;
+        FETCH c_suma_detalles_c INTO v_valor_gast_c;
+        CLOSE c_suma_detalles_c;
         --
         UPDATE im_tgast
-           SET gast_valor  = v_valor_gast
+           SET gast_valor  = (v_valor_gast_d-v_valor_gast_c)
          WHERE gast_gast =  NEW.dgas_gast
          ;
         --
