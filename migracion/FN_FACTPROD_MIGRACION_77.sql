a4afbf873be0d5e5643b9004ec3d5f39dbec9cf6
jmorenor1986 <johmorenoing@gmail.com>
2016-07-15 16:08:56 -0500

    Migracion esta re paila

--- migracion/FN_FACTPROD_MIGRACION_77.sql
+++ migracion/FN_FACTPROD_MIGRACION_77.sql
@@ -8,18 +8,57 @@ CREATE OR REPLACE FUNCTION FN_FACTPROD_MIGRACION_77(
                                                 p_cantidad      BIGINT,        --Cantidad de productos que se vendieron
                                                 p_idmvco        BIGINT,        --Id de los movimientos contables que generar la transaccion
                                                 p_fact          BIGINT,        --Id de facturacion 
-                                                p_precio        NUMERIC(1000,10)
+                                                p_precio        NUMERIC(1000,10),
+                                                v_vlr_iva_tot   NUMERIC(1000,10),
+                                                v_vlr_iva_uni   NUMERIC(1000,10),
+                                                v_vlr_uni_fact   NUMERIC(1000,10),
+                                                v_vlr_tot_fact_iva  NUMERIC(1000,10),
+                                                v_utilidad_prod     NUMERIC(1000,10)
+
                                             ) RETURNS VARCHAR  AS $$
 	DECLARE
 	    --
-    --Variable utilizada para almacenar la respuesta de la salida de inventario
-    --
-    v_rta_insrt_kar         VARCHAR(500) := '';
-    --
-    --Variable en la cual se almacenara el movimiento de inventario que se encuentra parametrizado para las ventas
-    --
+    
+    v_existencias           NUMERIC(1000,10) := 0;
+    v_exis_total            NUMERIC(1000,10) := 0;
+    v_valida_ex             VARCHAR(1000):='';
+    v_iva_precio            NUMERIC(1000,10) := 0;
+    v_auxiliar              NUMERIC(1000,10) := 100.000;
     v_mvin_mvin             INT :=0;
     v_val_mvin_fact         INT := 0;
+    v_kapr_kapr             NUMERIC(1000,10)  :=0;
+    v_rta_insrt_kar         VARCHAR(500) := '';
+    v_dtpr_dtpr             NUMERIC(1000,10);
+    v_vlr_prom_pond         NUMERIC(1000,10)   := 0;
+    v_vlr_prom_pond_tot     NUMERIC(1000,10) := 0;
+    v_sbcu_cod_prod         varchar(100) := '';
+
+    --
+    --Cursor con el cual obtengo las existencias de la sede
+    --
+    c_exit_x_sede CURSOR IS
+    SELECT eprs_existencia
+      FROM in_teprs
+     WHERE eprs_dska = p_dska
+       AND eprs_sede = p_sede
+       ;
+    --
+    --Cursor con el cual obtengo las existencias totales de un producto
+    --
+    c_exist_total CURSOR IS
+    SELECT cepr_existencia
+      FROM in_tcepr
+     WHERE cepr_dska = p_dska
+     ;
+
+    --
+    --Cursor que obtiene el iva parametrizado
+    --
+    c_iva_precio CURSOR IS
+    SELECT cast(para_valor as numeric)
+      FROM em_tpara
+     WHERE para_clave = 'IVAPRVENTA'
+    ;
 
     --
     --Moviento de inventario parametrizado para la venta de productos
@@ -29,10 +68,180 @@ CREATE OR REPLACE FUNCTION FN_FACTPROD_MIGRACION_77(
       FROM in_tmvin
      WHERE mvin_venta = 'S'
      ;
+    --  
+    --Valida si el movimiento de inventario para facturacion esta parametrizado
+    --
+    c_valida_movi_fact CURSOR FOR
+    SELECT count(*)
+      FROM in_tmvin
+     WHERE mvin_venta = 'S'
+     ;
+    --
+    -- Cursor con el cual encuentro el id del movimiento de inventario
+    --
+    c_kapr_kapr CURSOR (vc_expresion varchar) IS
+    SELECT cast(kapr_kapr as int)
+      FROM (
+           SELECT regexp_split_to_table(vc_expresion, '-') kapr_kapr
+           offset 1) as tabla
+    ;
+    --
+    --Cursor con el cual obtengo el id de la tabla fa_tdtpr (Detalles de facturacion de productos)
+    --
+    c_dtpr_dtpr CURSOR FOR
+    SELECT coalesce(max(dtpr_dtpr),0) + 1
+      from fa_tdtpr
+    ;
+
+     --
+    --Cursor con el cual obtengo el valor del promedio pornderado del producto
+    --
+    c_prom_pond_prod CURSOR(vc_dska_dska INT) IS
+    SELECT kapr_cost_mvto_uni, kapr_cant_saldo,kapr_cost_saldo_tot 
+    --SELECT kapr_cost_saldo_uni, kapr_cant_saldo,kapr_cost_saldo_tot -- Se cambia por que en el caso de que se venda la ultima existencia todos los valores daran en ceros
+      FROM in_tkapr
+     WHERE kapr_kapr = (select max(kapr_kapr) from in_tkapr where kapr_dska = vc_dska_dska)
+    ;
+    --
+    --Obtiene el codico de la subcuenta para un producto
+    --
+    c_cod_sbcu CURSOR (vc_dska_dska  INT) IS
+    SELECT sbcu_codigo
+      FROM co_tsbcu, in_tdska
+     WHERE dska_dska = vc_dska_dska
+       AND sbcu_sbcu = dska_sbcu
+     ;     
+
 	
 	BEGIN
 	
-	
+    OPEN c_exit_x_sede;
+    FETCH c_exit_x_sede INTO v_existencias;
+    CLOSE c_exit_x_sede;
+
+    --
+    --verifica las existencias en la sede
+    IF v_existencias < p_cantidad THEN
+
+        OPEN c_exist_total;
+        FETCH c_exist_total INTO v_exis_total;
+        CLOSE c_exist_total;
+
+         IF v_exis_total > p_cantidad THEN
+
+            v_valida_ex := IN_VALIDA_EXISTENCIAS(p_dska); 
+
+            OPEN c_exist_total;
+            FETCH c_exist_total INTO v_exis_total;
+            CLOSE c_exist_total;
+
+            IF v_exis_total > p_cantidad THEN
+                --
+                RAISE EXCEPTION 'En la sede en la cual esta facturando no hay las cantidades suficientes pero en el resto de la empresa si hay suficientes para satisfacer la compra, cantidad total de productos con codigo: 1-% , cantidades totales: % ',p_dska, v_exis_total;
+                --
+            ELSE
+                --
+                RAISE EXCEPTION 'No existe la cantidad de productos suficientes del producto con el codigo 1-% Las cantidades en toda la empresa actualmente son: % ', p_dska,v_exis_total;
+                --
+            END IF;
+            ELSE
+            --
+            RAISE EXCEPTION 'No existe la cantidad de productos suficientes del producto con el codigo 1-% Las cantidades en toda la empresa actualmente son: % ', p_dska,v_exis_total;
+
+         END IF;    
+    END IF;
+    --
+    --Calculo la base del iva con el iva parametrizado previamente
+    --
+    OPEN c_iva_precio;
+    FETCH c_iva_precio INTO v_iva_precio;
+    CLOSE c_iva_precio;
+
+    OPEN c_prom_pond_prod(p_dska);
+    FETCH c_prom_pond_prod INTO v_vlr_prom_pond;
+    CLOSE c_prom_pond_prod;
+    --
+    p_precio := (v_auxiliar * p_precio) / (v_auxiliar + v_iva_precio);
+
+    OPEN c_valida_movi_fact;
+    FETCH c_valida_movi_fact INTO v_val_mvin_fact;
+    CLOSE c_valida_movi_fact;
+    --
+    IF v_val_mvin_fact <> 1 THEN
+        --
+        RAISE EXCEPTION 'No existe ningun movimiento de inventario que referencie la facturacion de productos';
+        --
+    ELSE
+        --
+        OPEN c_mvin_venta;
+        FETCH c_mvin_venta INTO v_mvin_mvin;
+        CLOSE c_mvin_venta;
+
+    END IF;
+    --
+    --Realizamos la salida del inventario del producto 
+    --
+    v_rta_insrt_kar := IN_FINSERTA_PROD_KARDEX(p_dska,
+                                               v_mvin_mvin,
+                                               p_tius,
+                                               p_cantidad,
+                                               0,
+                                               p_sede                                                   
+                                               );
+
+    IF upper(v_rta_insrt_kar) NOT LIKE '%OK%' THEN
+        --
+        RAISE EXCEPTION 'Error al hacer la salida de inventario. % ', v_rta_insrt_kar ;
+        --
+    ELSE
+        --
+        OPEN c_kapr_kapr(v_rta_insrt_kar);
+        FETCH c_kapr_kapr INTO v_kapr_kapr;
+        CLOSE c_kapr_kapr;
+        --
+    END IF;
+        OPEN c_dtpr_dtpr;
+        FETCH c_dtpr_dtpr INTO v_dtpr_dtpr;
+        CLOSE c_dtpr_dtpr;
+
+
+       INSERT INTO fa_tdtpr(
+            dtpr_dtpr, dtpr_dska, dtpr_fact, 
+            dtpr_num_prod, dtpr_cant, dtpr_vlr_pr_tot, 
+            dtpr_vlr_uni_prod, dtpr_vlr_iva_tot, dtpr_vlr_iva_uni, 
+            dtpr_vlr_venta_tot, dtpr_vlr_venta_uni, dtpr_vlr_total, 
+            dtpr_desc, dtpr_kapr,dtpr_valor_desc,dtpr_utilidad,dtpr_con_desc)
+        VALUES (
+            v_dtpr_dtpr, p_dska, p_fact, 
+            0, p_cantidad, p_precio*p_cantidad, 
+            p_precio, v_vlr_iva_tot, v_vlr_iva_uni, 
+            v_vlr_tot_fact_iva, v_vlr_uni_fact, v_vlr_tot_fact_iva,
+            'N', v_kapr_kapr,0,v_utilidad_prod, 'N');
+
+    OPEN c_cod_sbcu(p_dska);
+    FETCH c_cod_sbcu INTO v_sbcu_cod_prod;
+    CLOSE c_cod_sbcu;
+
+     --
+    --Calculo el promedio ponderado total de todos los productos
+    --
+    v_vlr_prom_pond_tot := v_vlr_prom_pond * p_cantidad;   
+    
+
+     --
+    --Insercion para que se contabilice la salida del producto
+    --
+    INSERT INTO co_ttem_mvco(
+            tem_mvco_trans, tem_mvco_sbcu, tem_mvco_valor, tem_mvco_naturaleza)
+    VALUES (p_idmvco, v_sbcu_cod_prod , v_vlr_prom_pond_tot , 'C');
+    --
+    --Insercion para la entrada de costo de ventas
+    --
+    INSERT INTO co_ttem_mvco(
+            tem_mvco_trans, tem_mvco_sbcu, tem_mvco_valor, tem_mvco_naturaleza)
+    VALUES (p_idmvco, '613535' , v_vlr_prom_pond_tot , 'D');
+    --  
+    RETURN 'OK';
 	
 	EXCEPTION WHEN OTHERS THEN
       RETURN 'Error FA_FACTURA_PRODUCTO '|| sqlerrm;
--- migracion/FN_MIGRACION_77.sql
+++ migracion/FN_MIGRACION_77.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCION FN_MIGRACION_77()
+CREATE OR REPLACE FUNCTION FN_MIGRACION_77()
 								RETURNS VARCHAR  AS $$
 	DECLARE
 	
@@ -18,13 +18,57 @@ CREATE OR REPLACE FUNCION FN_MIGRACION_77()
 	--
 	--cursor con el cual obtengo los datos de los productos
 	--
-	c_cons_prod_migt(v_fact_fact BIGINT) CURSOR FOR
-	SELECT dtprMIG_dska,dtprMIG_cant,dtprMIG_vlr_uni_prod
+	c_cons_prod_migt CURSOR (v_fact_fact BIGINT)  IS
+	SELECT dtprMIG_dska,dtprMIG_cant,dtprMIG_vlr_uni_prod,factMIG_vlr_iva,factMIG_vlr_total
 	FROM fa_tdtprMIG 
 	WHERE dtprMIG_fact = v_fact_fact;
 	--
+	--
+    --Cursor con el cual obtenemos el valor total del iva de la factura
+    --
+    c_vlr_iva_fact CURSOR(vc_fact_fact INT) FOR
+    SELECT fact_vlr_iva
+      FROM fa_tfact
+     WHERE fact_fact = vc_fact_fact
+     ;
+    --
+    --Cursor con el cual obtnemos el valor de la factura sin iva
+    --
+    c_vlr_total_fact_sin_iva CURSOR(vc_fact_fact INT) FOR
+    SELECT fact_vlr_total, 
+      FROM fa_tfact
+     WHERE fact_fact = vc_fact_fact
+     ;
+    --
+    --Cursor con el cual se obtiene el id de la subcuenta de la caja menor
+    --
+    c_sbcu_caja CURSOR FOR
+    SELECT sbcu_codigo
+      FROM em_tsede, co_tsbcu
+     WHERE sede_sede = p_sede
+       AND sbcu_sbcu = sede_sbcu_caja
+     ; 
 	--variables
 	v_idTrans_con           BIGINT := 0;
+	v_rta_fact_prod			VARCHAR(500)='';
+	v_valor_iva_fact        NUMERIC(1000,10) := 0;
+    v_vlr_total_factura     NUMERIC(1000,10) := 0;
+    v_vlr_total_fact_co     NUMERIC(1000,10) := 0;
+    --
+    --Variables utilizadas para los valores principales de facturacion
+    --
+    v_vlr_total     NUMERIC(1000,10)  :=0;
+    v_vlr_iva       NUMERIC(1000,10)  :=0;
+    v_vlr_retfuente NUMERIC(1000,10)  :=0;
+    v_sbcu_caja_cod     varchar(10):= '';
+    --
+    --Variables necesarias para la validacion de subcuentas
+    --
+    v_val_iva_generado          bigint :=0;
+    v_val_costo_ventas          bigint :=0;
+    v_val_mercancias_mm         bigint :=0;
+    v_val_descuentos            bigint :=0;
+    v_val_caja_menor            bigint :=0;
 		BEGIN
 		
 
@@ -33,7 +77,7 @@ CREATE OR REPLACE FUNCION FN_MIGRACION_77()
 		 	--
 			--Insertar facturas
 			INSERT INTO FA_TFACT(fact_fact		   ,  fact_tius	    		,  fact_fec_ini		   , 
-								fact_clien		   , fact_vlr_total		   	, fact_vlr_iva		   ,)
+								fact_clien		   , fact_vlr_total		   	, fact_vlr_iva		   )
                      VALUES (	fact.factMIG_fact  , fact.factMIG_tius      ,  fact.factMIG_fec_ini, 
 								fact.factMIG_clien , fact.factMIG_vlr_total , fac.factMIG_vlr_iva   )
 							;
@@ -54,11 +98,39 @@ CREATE OR REPLACE FUNCION FN_MIGRACION_77()
                                                 prod.dtprMIG_cant,
                                                 v_idTrans_con,
                                                 cast(fact.factMIG_fact as int),
-                                                prod.dtprMIG_vlr_uni_prod
+                                                prod.dtprMIG_vlr_uni_prod,
+                                                prod.dtprMIG_vlr_iva_tot,
+                                                prod.dtprMIG_vlr_iva_uni,
+                                                prod.dtprMIG_vlr_total,
+                                                prod.dtpr_vlr_venta_tot,
+                                                prod.dtprMIG_utilidad	
                                                 );
-			END LOOP;		
+			END LOOP;
+
+			OPEN c_vlr_iva_fact(cast(v_fact_fact as int));
+		    FETCH c_vlr_iva_fact INTO v_valor_iva_fact;  
+		    CLOSE c_vlr_iva_fact;
+
+		     INSERT INTO co_ttem_mvco(
+            tem_mvco_trans, tem_mvco_sbcu, tem_mvco_valor, tem_mvco_naturaleza)
+		    VALUES (v_idTrans_con, '240802' , v_valor_iva_fact , 'C');
+		    --
+
+
+		    INSERT INTO co_ttem_mvco(
+            tem_mvco_trans, tem_mvco_sbcu, tem_mvco_valor, tem_mvco_naturaleza)
+    		VALUES (v_idTrans_con, '413535' , v_vlr_total_factura , 'C');
+
+    		    --
+		    --Logica para que el dinero valla directo a la caja menor
+		    --
+		    OPEN c_sbcu_caja;
+		    FETCH c_sbcu_caja INTO v_sbcu_caja_cod;
+		    CLOSE c_sbcu_caja;		
 		 
 		 END LOOP;
+
+    --
 	
 		EXCEPTION WHEN OTHERS THEN
 			 RETURN 'Error FA_REGISTRA_FACT_COMPRA '|| sqlerrm;
