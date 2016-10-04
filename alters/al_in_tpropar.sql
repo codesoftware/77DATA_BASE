ef75f4d902bdcfbf96f6f9f1857ccb69978429e7
jmorenor1986 <johnmorenoing@gmail.com>
2016-10-04 17:19:34 -0500

    Productos inventareables
    Se añade los campos para las cantidades inventareables y no
    inventareables.

---
+++ alters/al_in_tpropar.sql
@@ -0,0 +1,4 @@
+alter table FA_TFCPRD_TMP add  fcprd_tmp_inve varchar(2);
+alter table FA_TFCPRD_TMP add  fcprd_tmp_cantinve bigint;
+alter table FA_TFCPRD add  fcprd_inve varchar(2);
+alter table FA_TFCPRD add  fcprd_cantinve bigint;
--- tables/co_ttem_mvco.sql
+++ tables/co_ttem_mvco.sql
@@ -7,5 +7,6 @@ CREATE TABLE CO_TTEM_MVCO(
     TEM_MVCO_SBCU                  VARCHAR(10)         NOT NULL                   ,
     TEM_MVCO_VALOR                 VARCHAR(250)        NOT NULL                   ,
     TEM_MVCO_NATURALEZA            VARCHAR(2)          NOT NULL                   ,
-    TEM_MVCO_PROV                  bigint                             
+    TEM_MVCO_PROV                  bigint                                         ,
+    TEM_MVCO_AUCO                  bigint              NOT NULL DEFAULT -1
 );
